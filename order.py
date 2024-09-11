cf485911322f8c0d72c7e0529274d3a6825af954
Dheeraj Raina <dheerajraina55@gmail.com>
2024-09-11 12:44:15 +0530

    Initial Order Definition

---
+++ order.py
@@ -0,0 +1,15 @@
+import time
+
+
+class Order:
+    def __init__(self, order_id, price, quantity, side, timestamp=None, order_type='limit'):
+        self.order_id = order_id
+        self.side = side  # buy or sell
+        self.price = price  # price at which order is placed
+        self.quantity = quantity
+        self.order_type = order_type  # limit,market,stop
+        self.timestamp = timestamp
+
+
+def __repr__(self):
+    return f"Order(id={self.order_id},price={self.price},quantity={self.quantity},side={self.side},type={self.order_type})"
