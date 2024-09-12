4fe83d0b1ba8e8f591638cb4aef228d8eeaa6cd6
Dheeraj Raina <dheerajraina55@gmail.com>
2024-09-12 12:12:14 +0530

    Added test for order book display by push buy and sell orders concurrently

---
+++ tests.py
@@ -0,0 +1,41 @@
+import random
+import time
+import threading
+from order import Order
+from orderbook import OrderBook
+
+
+def test_order_book_display():
+    order_book = OrderBook()
+
+    def push_buy_orders():
+        for i in range(1000000):
+            time.sleep(1)
+            price = random.randint(0, 100)
+            quantity = random.randint(0, 1000)
+            new_order = Order(
+                order_id=f'buy{i}', price=price, quantity=quantity, side='buy')
+            order_book.handle_new_order(new_order)
+            order_book.display_order_book()
+
+    def push_sell_orders():
+        for i in range(1000000):
+            time.sleep(1)
+            price = random.randint(0, 100)
+            quantity = random.randint(0, 1000)
+            new_order = Order(
+                order_id=f'sell_{i}', price=price, quantity=quantity, side='sell')
+            order_book.handle_new_order(new_order)
+            order_book.display_order_book()
+
+    thread_buy = threading.Thread(target=push_buy_orders)
+    thread_sell = threading.Thread(target=push_sell_orders)
+
+    thread_buy.start()
+    thread_sell.start()
+
+    thread_buy.join()
+    thread_sell.join()
+
+
+test_order_book_display()
