6abcd62fff0fb4088ef1e94ab95fc72b5057a623
Dheeraj Raina <dheerajraina55@gmail.com>
2024-09-12 12:03:08 +0530

    Added function to handle new orders and a function to display order book as new orders are pushed to it

--- orderbook.py
+++ orderbook.py
@@ -15,3 +15,33 @@ class OrderBook:
         else:
             heapq.heappush(self.sell_orders,
                            (order.price, order.timestamp, order))
+
+    def display_order_book(self):
+        print(
+            f"{'Price':<10}{'Side':<8}{'Quantity':<10}{'|':<3}{'Price':<10}{'Side':<8}{'Quantity'}")
+        print("-" * 50)
+
+        # -price for descending
+        buy_orders = sorted(
+            self.buy_orders, key=lambda x: (-x[0], x[1]), reverse=True)
+        sell_orders = sorted(self.sell_orders, key=lambda x: (
+            x[0], x[1]))
+
+        # Max length b/w buy and sell for row alignment
+        max_len = max(len(buy_orders), len(sell_orders))
+        for i in range(max_len):
+            buy_line = ""
+            sell_line = ""
+
+            if i < len(buy_orders):
+                buy_order = buy_orders[i][2]  # Get the actual Order object
+                buy_line = f"{buy_order.price:<10}{'BUY':<8}{buy_order.quantity:<10}"
+
+            if i < len(sell_orders):
+                sell_order = sell_orders[i][2]  # Get the actual Order object
+                sell_line = f"{sell_order.price:<10}{'SELL':<8}{sell_order.quantity:<10}"
+
+            print(f"{buy_line:<30}| {sell_line}")
+
+    def handle_new_order(self, order):
+        self.add_order(order)
