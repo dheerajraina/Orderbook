41d5b356072e4f0a6e0e91703f2c189be690b3de
Dheeraj Raina <dheerajraina55@gmail.com>
2024-09-12 14:40:34 +0530

    minor updates

--- tests.py
+++ tests.py
@@ -9,25 +9,25 @@ def test_order_book():
     order_book = OrderBook()
 
     def push_buy_orders():
-        for i in range(100):
-            time.sleep(1)
-            price = random.randint(0, 100)
-            quantity = random.randint(0, 1000)
+        for i in range(1000):
+            time.sleep(0.1)
+            price = random.randint(1, 100)
+            quantity = random.randint(1, 1000)
             order_type = random.choice(['market', 'limit', 'stop'])
             new_order = Order(
                 order_id=f'buy{i}', price=price, quantity=quantity, side='buy', order_type=order_type)
             order_book.handle_new_order(new_order)
-            order_book.display_order_book()
 
     def push_sell_orders():
-        for i in range(100):
-            time.sleep(1)
+        for i in range(1000):
+            time.sleep(0.1)
             price = random.randint(0, 100)
-            quantity = random.randint(0, 1000)
+            quantity = random.randint(1, 1000)
             order_type = random.choice(['market', 'limit', 'stop'])
             new_order = Order(
                 order_id=f'sell_{i}', price=price, quantity=quantity, side='sell', order_type=order_type)
             order_book.handle_new_order(new_order)
+
             order_book.display_order_book()
 
     thread_buy = threading.Thread(target=push_buy_orders)
