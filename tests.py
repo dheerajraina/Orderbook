6003999346eada519155cfb66bab63317245be18
Dheeraj Raina <dheerajraina55@gmail.com>
2024-09-12 13:02:29 +0530

    test for order matching(order book display integrated in it as well)

--- tests.py
+++ tests.py
@@ -5,26 +5,28 @@ from order import Order
 from orderbook import OrderBook
 
 
-def test_order_book_display():
+def test_order_book():
     order_book = OrderBook()
 
     def push_buy_orders():
-        for i in range(1000000):
+        for i in range(100):
             time.sleep(1)
             price = random.randint(0, 100)
             quantity = random.randint(0, 1000)
+            order_type = random.choice(['market', 'limit', 'stop'])
             new_order = Order(
-                order_id=f'buy{i}', price=price, quantity=quantity, side='buy')
+                order_id=f'buy{i}', price=price, quantity=quantity, side='buy', order_type=order_type)
             order_book.handle_new_order(new_order)
             order_book.display_order_book()
 
     def push_sell_orders():
-        for i in range(1000000):
+        for i in range(100):
             time.sleep(1)
             price = random.randint(0, 100)
             quantity = random.randint(0, 1000)
+            order_type = random.choice(['market', 'limit', 'stop'])
             new_order = Order(
-                order_id=f'sell_{i}', price=price, quantity=quantity, side='sell')
+                order_id=f'sell_{i}', price=price, quantity=quantity, side='sell', order_type=order_type)
             order_book.handle_new_order(new_order)
             order_book.display_order_book()
 
@@ -37,5 +39,7 @@ def test_order_book_display():
     thread_buy.join()
     thread_sell.join()
 
+    print(f"Executed {len(order_book.trades)} trades")
 
-test_order_book_display()
+
+test_order_book()
